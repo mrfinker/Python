e2650ef388ec6bd7a926a4e530f610fbed6844a2
mrfinker <caalebs@gmail.com>
2022-05-03 12:54:59 +0000

    2

---
+++ Projects/Apps/Learing/1_Basics/6_1_Projet.py
@@ -0,0 +1,15 @@
+### Temps de vol
+# Vous devez calculer le temps de vol d'un voyage à venir.
+# Vous voyagez de Los Angeles à Sydney, couvrant une distance de 7425 miles, l'avion vole à une vitesse moyenne de 550 miles à l'heure.
+# Calculez et affichez le temps de vol total en heures.
+
+## Indice
+# Le résultat devrait être un flotteur.
+# Utilisez l'instruction print pour afficher le résultat.
+
+distance = 7425
+vitesse = 550
+
+temp_voyage = distance/vitesse
+
+print (temp_voyage)
