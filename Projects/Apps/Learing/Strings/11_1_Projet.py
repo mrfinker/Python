34e76eb504b5789c5a7cb4c63a2eb9fcabf26cd0
mrfinker <caalebs@gmail.com>
2022-05-03 16:39:36 +0000

    Mardi

---
+++ Projects/Apps/Learing/Strings/11_1_Projet.py
@@ -0,0 +1,9 @@
+#Vous devez créer un programme pour un classement.
+#Le programme doit afficher les chiffres de 1 à 9, chacun sur une ligne distincte, suivis d'un point :
+# 1.
+# 2.
+# 3.
+# ...
+# Vous pouvez utiliser le caractère de saut de ligne \n pour créer des sauts de ligne ou, alternativement, créer la sortie souhaitée en utilisant trois guillemets doubles """.
+
+print("1.\n2.\n3.\n4.\n5.\n6.\n7.\n8.\n9.\n")
