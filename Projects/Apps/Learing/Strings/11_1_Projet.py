5f573e2bd58735ab8d40516a525e348854b0e832
mrfinker <caalebs@gmail.com>
2022-05-04 23:07:32 +0000

    mer

--- Projects/Apps/Learing/Strings/11_1_Projet.py
+++ Projects/Apps/Learing/Strings/11_1_Projet.py
@@ -3,7 +3,6 @@
 # 1.
 # 2.
 # 3.
-# ...
 # Vous pouvez utiliser le caractère de saut de ligne \n pour créer des sauts de ligne ou, alternativement, créer la sortie souhaitée en utilisant trois guillemets doubles """.
 
 print("1.\n2.\n3.\n4.\n5.\n6.\n7.\n8.\n9.\n")
