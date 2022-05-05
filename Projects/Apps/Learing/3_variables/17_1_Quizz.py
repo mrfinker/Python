4a8101547c9b5c34492e20ae54aef52a299e43b8
mrfinker <caalebs@gmail.com>
2022-05-05 12:33:13 +0000

    jeudi

---
+++ Projects/Apps/Learing/3_variables/17_1_Quizz.py
@@ -0,0 +1,22 @@
+# que donne ce code
+
+pov = "6"
+pov = pov + "0"
+basse = int(pov) + 3
+print(float(basse))
+print(basse)
+print()
+
+age = int(input("Entrer entier :"))
+print(age + 8)
+print()
+
+x = 5
+y = x + 3
+y = int(str(y) + "2")
+print(y)
+print()
+
+num_1 = 3
+num_2 = 177
+print(num_2 % num_1)
