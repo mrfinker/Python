ca923d70c92cdfa66c9ada22c08ccc7e65eb00f7
mrfinker <caalebs@gmail.com>
2022-05-05 10:29:58 +0000

    jeudi

---
+++ Projects/Apps/Learing/3_variables/14_1_input.py
@@ -0,0 +1,21 @@
+# La fonction input() demande l'utilisateur à entrer une valeur et renvoie l'entree en tant que chaine 'retainez bien en tant que chaine'
+# mme si l'utilisateur entre un entier il sera traiter comme une chaine de caractere
+# la declaration d'entree doit etre suivie par les parentheses
+
+x = input()
+print(x)
+print()
+
+y = input()
+print("Vous venez d'entrer : " + y)
+print()
+
+nom = input()
+print("Bonjour, " + nom)
+print()
+
+# on peut mettre une chaine de message dans la parenthese
+# Le message indiquera a l'utilisateur ce qu'il devra faire
+
+nom_1 = input("Entrer votre nom : ")
+print = ("Bienvenue, " + nom_1)
---
+++ Projects/Apps/Learing/3_variables/14_2_mini_projet.py
@@ -0,0 +1,4 @@
+# ecrire un code pour prendre a l'entree une chaine et la sortie qui sera repeter 10 fois
+
+x = input("Entrer une chaine de caractere : ")
+print( x * 10)
