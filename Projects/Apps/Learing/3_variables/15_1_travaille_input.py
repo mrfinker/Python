2c96c06d1cd0af453c63ca962d7a2a7d8984b959
mrfinker <caalebs@gmail.com>
2022-05-05 10:46:08 +0000

    jeudi

---
+++ Projects/Apps/Learing/3_variables/15_1_travaille_input.py
@@ -0,0 +1,12 @@
+# la fonction input() renvoie une chaine de caractere
+# pour le convertir en entier  c'est simple, nous utiliserons la fonction --- int(inut())
+
+age = int(input())
+print(type(age))
+print(age)
+print()
+
+age_1 = input()
+print(type(age_1))
+int(age_1)
+print(type(age_1))
